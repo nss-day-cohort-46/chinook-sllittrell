47fe496d6536475d3c53d50d039b7df86d162468
sllittrell <stacey.littrell@gmail.com>
2021-09-09 10:48:05 -0500

    edit to top_country & top_media

--- exercises/18-23/top_country.sql
+++ exercises/18-23/top_country.sql
@@ -1,4 +1,4 @@
-SELECT Country, MAX(totalSale) saleAmount
+SELECT Country, '$'||MAX(totalSale) saleAmount
 FROM (
 SELECT BillingCountry Country , sum(i.total) totalSale FROM Invoice i
 GROUP BY BillingCountry)
